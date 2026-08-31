b910c73c05324e1335c0194b87b0c4de0ecf749c
shvedovteam <shvedovteam@gmail.com>
2026-08-31 16:50:29 +0300

    feat: expose admin backoffice route

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Admin from './Admin'
+
+const isAdmin = window.location.pathname.startsWith('/admin')
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {isAdmin ? <Admin /> : <App />}
   </React.StrictMode>,
 )
